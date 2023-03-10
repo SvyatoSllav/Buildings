1a500ae69acfe348e2efc81a56ae01e69cf15989
Александр <tarasov.slavas2002@gmail.com>
2023-03-10 20:33:10 +0000

    Добавил модальное окно

--- js/main.js
+++ js/main.js
@@ -12,3 +12,35 @@ burgerMenu.onclick = () => {
         sidebar.classList.add("sidebar-nav_hide")
     }
 }
+function showContactModal() {
+    if (wrapper.classList.contains("hide")) {
+        wrapper.style.zIndex = "2"
+        wrapper.style.visibility = "visible"
+        contactModal.style.visibility = "visible"
+        overlay.style.visibility = "visible"
+        wrapper.classList.remove("hide")
+    } else {
+        wrapper.style.zIndex = "1"
+        wrapper.style.visibility = "hidden"
+        contactModal.style.visibility = "hidden"
+        overlay.style.visibility = "hidden"
+        wrapper.classList.add("hide")
+    }
+}
+function hideContactModal() {
+    wrapper.style.zIndex = "1"
+    wrapper.style.visibility = "hidden"
+    contactModal.style.visibility = "hidden"
+    overlay.style.visibility = "hidden"
+    wrapper.classList.add("hide")
+}
+
+contactButtons = document.body.querySelectorAll(".request__send-mail")
+contactButtons.forEach(contactButton => {
+    contactButton.addEventListener("click", showContactModal)
+})
+
+wrapper = document.querySelector(".contact-modal-wrapper")
+contactModal = document.querySelector(".contact-modal")
+overlay = document.querySelector(".overlay")
+overlay.addEventListener("click", hideContactModal)
