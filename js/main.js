18f5f6613a8a666e463f3de23c3dd56682eda471
Александр <tarasov.slavas2002@gmail.com>
2023-03-10 21:05:59 +0000

    Фиксы для адаптивности

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-burgerMenu = document.querySelector(".burger")
-burgerContainer = burgerMenu.closest(".container")
-burgerMenu.style.right = (document.body.clientWidth - burgerContainer.offsetWidth) / 2 - 60 + "px"
-
+let burgerMenu = document.querySelector(".burger")
+let burgerContainer = burgerMenu.closest(".container")
+let containerMarginRight = window.getComputedStyle(burgerContainer).marginRight
+console.log(parseInt(containerMarginRight) * 2)
+// burgerMenu.style.right = (document.body.clientWidth - burgerContainer.offsetWidth) / 2 - 60 + "px"
+burgerMenu.style.right = parseInt(containerMarginRight) + "px"
+console.log(burgerMenu.style.right)
+// console.log(burgerContainer.offsetWidth)
 burgerMenu.onclick = () => {
-    console.log(document.body.clientWidth)
-    sidebar = document.querySelector(".sidebar-nav")
-    console.log(sidebar)
+    let sidebar = document.querySelector(".sidebar-nav")
     if (sidebar.classList.contains("sidebar-nav_hide")) {
         sidebar.classList.remove("sidebar-nav_hide")
     } else {
@@ -35,12 +37,12 @@ function hideContactModal() {
     wrapper.classList.add("hide")
 }
 
-contactButtons = document.body.querySelectorAll(".request__send-mail")
+let contactButtons = document.body.querySelectorAll(".request__send-mail")
 contactButtons.forEach(contactButton => {
     contactButton.addEventListener("click", showContactModal)
 })
 
-wrapper = document.querySelector(".contact-modal-wrapper")
-contactModal = document.querySelector(".contact-modal")
-overlay = document.querySelector(".overlay")
+let wrapper = document.querySelector(".contact-modal-wrapper")
+let contactModal = document.querySelector(".contact-modal")
+let overlay = document.querySelector(".overlay")
 overlay.addEventListener("click", hideContactModal)
